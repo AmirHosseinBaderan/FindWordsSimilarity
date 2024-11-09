b61e9cb2ef15ee2758247c72c03f3fdd97f52773
amir hossin <65053996+AmirHosseinBaderan@users.noreply.github.com>
2024-11-09 22:26:57 +0330

    Update Constants.cs

--- StringSimilarity/Constants/Constants.cs
+++ StringSimilarity/Constants/Constants.cs
@@ -2,7 +2,7 @@
 
 public class Constants
 {
-    public const string ConnectionString = "data source=.;initial catalog=GroupsDb;User ID=sa;Password=2H13okXCMssql;TrustServerCertificate=True;";
+    public const string ConnectionString = "data source=.;initial catalog=GroupsDb;Integrated Security=True;TrustServerCertificate=True;";
 
     public const string TableName = "ProductCategories";
 }
